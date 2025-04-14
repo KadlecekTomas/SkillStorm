8e5af6dde816906546a9b2ff3009a2fcdc5f599d
Tomas Kadlecek <kadle.tom@gmail.com>
2025-04-14 15:40:08 +0200

    feat: generate modules and base structure for user, test, question, submission, response

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TestModule } from './test/test.module';
+import { QuestionModule } from './question/question.module';
+import { SubmissionModule } from './submission/submission.module';
+import { ResponseModule } from './response/response.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, TestModule, QuestionModule, SubmissionModule, ResponseModule],
   controllers: [AppController],
   providers: [AppService],
 })
